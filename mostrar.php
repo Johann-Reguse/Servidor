1d3f0710153d762b380b4433d3b97f6e1f99e9dc
Johann-Reguse <51337281+Johann-Reguse@users.noreply.github.com>
2022-07-24 18:31:55 -0300

    Update mostrar.php

--- mostrar.php
+++ mostrar.php
@@ -1,10 +1,11 @@
 <?php
 session_start();
-echo('<h3>Livros cadastrados:<\h3>');
+
 foreach($_SESSION['Livro'] as $val){
   $oLivro = json_decode($val);
   echo($oLivro);
   echo('<br>');
 }
 echo('foi');
+echo('<h3>Livros cadastrados:<\h3>');
 ?>
