bc79b47256c388c9a595e0e5d4ad308033c74da7
Johann-Reguse <51337281+Johann-Reguse@users.noreply.github.com>
2022-08-01 09:58:43 -0300

    Create page.php

---
+++ page.php
@@ -0,0 +1,32 @@
+<?php
+	$dadosJson = file_get_contents('db.json');
+	$dadosJsonDecodificados = json_decode($dadosJson, true);
+	$method = $_SERVER['REQUEST_METHOD'];		
+
+	if($method === 'POST') {
+		echo "Livro enviado";
+		$values = file_get_contents('php://input');
+		print_r($value);
+		$dadosJsonDecodificados["Livro"][] = $value;
+		$fp = fopen('db.json', 'w');
+		fwrite($fp, json_encode($dadosJsonDecodificados));
+		fclose($fp);
+	}else {
+		if($method === 'GET') {
+			if ($_GET['isbn'] == null) {
+				print_r($dadosJson);
+			} else {
+				foreach ($dadosJsonDecodificados["Livro"] as $key => $value) {
+					foreach($value as $chave => $valor) {
+    						if($valor == $_GET['isbn']) {
+							$retorna = json_encode($value); 
+							print_r($retorna);
+						}
+					}		
+				}		
+			}
+		
+		}
+	}
+
+?>
